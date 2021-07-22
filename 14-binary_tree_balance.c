57889fdd1a45c51f5ed5c266175c9858b630a846
joseheco <jferhc@gmail.com>
2021-07-22 18:05:15 -0500

    binary

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,56 +1,62 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- * Return: The balance factor of the tree. If tree is NULL, return 0
+ * binary_tree_height1 - a function to get the height of the binary tree
+ * @tree: a pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: the node
  */
+
+
+size_t binary_tree_height1(const binary_tree_t *tree)
+{
+	size_t right_side = 0;
+	size_t left_side = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+
+	left_side = 1 + binary_tree_height1(tree->left);
+	right_side = 1 + binary_tree_height1(tree->right);
+
+	if (left_side > right_side)
+	{
+		return (left_side);
+	}
+
+	return (right_side);
+}
+
+
+/**
+ * binary_tree_balance - A function that measures the balance
+ * factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: 0 if tree is NULL otherwise 1
+ */
+
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int diff;
+	size_t height_left = 0, height_right = 0;
 
-	if (tree == NULL)
+	if (!tree)
 	{
 		return (0);
 	}
 
-	diff = 0;
-	diff = calc_diff(tree, tree, diff);
-	return (diff);
-}
-
-/**
- * calc_diff - calculates the difference between left and right branches
- * @root: Root node of the tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- * @diff: Difference between the left and right branches
- * Return: Difference between the left and right branches
- */
-
-int calc_diff(const binary_tree_t *root, const binary_tree_t *tree, int diff)
-{
-	int right;
-	int left;
-
-	right = diff;
-	left = diff;
-
-	if (tree == NULL)
+	if (tree->left)
 	{
-		return (0);
+		height_left = 1 + binary_tree_height1(tree->left);
 	}
 
-	left = left + calc_diff(root, tree->left, left);
-	right = right + calc_diff(root, tree->right, right);
+	if (tree->right)
+	{
+		height_right = 1 + binary_tree_height1(tree->right);
+	}
 
-	if (root == tree)
-	{
-		return (left - right);
-	}
-	if (right > left)
-	{
-		return (right + 1);
-	}
-	return (left + 1);
+	return (height_left - height_right);
 }
